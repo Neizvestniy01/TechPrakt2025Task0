933fb7a0a9db1528329f16a0788d169785d87af6
IlliaMarkevych <ipz231_miv@student.ztu.edu.ua>
2025-01-22 17:25:02 +0200

    Додання C# додатку

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("First number:");
+        string input1 = Console.ReadLine();
+        Console.WriteLine("Second number:");
+        string input2 = Console.ReadLine();
+        if (int.TryParse(input1, out int number1) && int.TryParse(input2, out int number2))
+        {
+            int sum = number1 + number2;
+            Console.WriteLine($"{number1} + {number2} = {sum}.");
+        }
+        else
+        {
+            Console.WriteLine("Error.");
+        }
+    }
+}
